9e02b09e0897b8e0d0d06e259b6f149db26c354a
伊扎利斯的魔女 <zhaoyu.wang@aminer.cn>
2025-05-19 20:54:50 +0800

    fix data

--- cmd/card-maker/main.go
+++ cmd/card-maker/main.go
@@ -4,12 +4,18 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"image"
+	"image/color"
+	"math"
 	"os"
 
+	"github.com/fogleman/gg"
 	"github.com/wzyjerry/harmonies/pkg/types"
 	"google.golang.org/protobuf/encoding/protojson"
 	"gopkg.in/yaml.v3"
 )
+
+var data types.GameData
 
 func yaml2json(b []byte) ([]byte, error) {
 	var data map[string]any
@@ -20,8 +26,7 @@ func yaml2json(b []byte) ([]byte, error) {
 	return json.Marshal(data)
 }
 
-func main() {
-	var data types.GameData
+func init() {
 	file, err := os.ReadFile("../../data/animals.yaml")
 	if err != nil {
 		panic(err)
@@ -34,9 +39,144 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	for _, animal := range data.Animals {
-		fmt.Println(PrintCard(animal))
+}
+
+const (
+	w               = 300
+	h               = 520
+	size            = 40
+	width           = 2 * size
+	padding         = 2
+	sizeWithPadding = size + padding
+	horiz           = sizeWithPadding * 3 / 2
+)
+
+var (
+	vert = math.Sqrt(3) * sizeWithPadding
+)
+
+func DrawTile() image.Image {
+	dc := gg.NewContext(width, width)
+	dc.SetRGBA255(0, 0, 0, 0)
+	dc.Clear()
+	dc.DrawRegularPolygon(6, width/2, width/2, size, 0)
+	dc.SetRGBA255(241, 211, 161, 255)
+	dc.Fill()
+	return dc.Image()
+}
+
+func GetPatternSize(pattern []*types.Token) (minQ, maxQ, minR, maxR int) {
+	minQ, maxQ = math.MaxInt, math.MinInt
+	minR, maxR = math.MaxInt, math.MinInt
+	for _, token := range pattern {
+		if int(token.DeltaQ) < minQ {
+			minQ = int(token.DeltaQ)
+		}
+		if int(token.DeltaQ) > maxQ {
+			maxQ = int(token.DeltaQ)
+		}
+		if int(token.DeltaR) < minR {
+			minR = int(token.DeltaR)
+		}
+		if int(token.DeltaR) > maxR {
+			maxR = int(token.DeltaR)
+		}
 	}
+	return
+}
+
+func DrawTileWithQR(dc *gg.Context, tile image.Image, q int, r int) {
+	// dc.DrawImageAnchored(tile, w/2+q*horiz+30, h/2+int(float64(2*r+q)*vert/2)+180, 0.5, 0.5)
+	dc.DrawImageAnchored(tile, w/2+q*horiz, h/2+int(float64(2*r+q)*vert/2), 0.5, 0.5)
+}
+
+func DrawToken(col color.Color, layer int) image.Image {
+	const cx, cy = width / 2, width / 2
+	dc := gg.NewContext(width, width)
+	dc.SetRGBA255(0, 0, 0, 0)
+	dc.Clear()
+	const r = size - 20 // 椭圆半径
+	height := 15.0
+	dc.Translate(0, 5-float64(layer)*height)
+
+	// 底面边线
+	dc.SetColor(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+	dc.DrawEllipse(cx, cy+height, r, r/2)
+	dc.Stroke()
+
+	// 侧面
+	dc.SetColor(col)
+	dc.DrawRectangle(cx-r, cy, r*2, height)
+	dc.FillPreserve()
+	dc.SetColor(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+	dc.Stroke()
+
+	// 底面填充
+	dc.SetColor(col)
+	dc.DrawEllipse(cx, cy+height, r, r/2)
+	dc.Fill()
+
+	// 顶面
+	dc.SetColor(col)
+	dc.DrawEllipse(cx, cy, r, r/2)
+	dc.FillPreserve()
+	// 暗部
+	dc.SetColor(color.RGBA{R: 0, G: 0, B: 0, A: 50})
+	dc.FillPreserve()
+	// 边线
+	dc.SetColor(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+	dc.Stroke()
+
+	return dc.Image()
+}
+
+func main() {
+	colors := map[types.Color]color.RGBA{
+		types.Color_ColorRed:    {R: 216, G: 40, B: 75, A: 255},
+		types.Color_ColorGreen:  {R: 110, G: 182, B: 46, A: 255},
+		types.Color_ColorBlue:   {R: 62, G: 126, B: 145, A: 255},
+		types.Color_ColorYellow: {R: 251, G: 175, B: 37, A: 255},
+		types.Color_ColorBrown:  {R: 111, G: 62, B: 59, A: 255},
+		types.Color_ColorGray:   {R: 120, G: 103, B: 102, A: 255},
+	}
+	dc := gg.NewContext(w, h)
+
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+
+	dc.Translate(w/2, h/2)
+	dc.Scale(1, 0.866)
+	dc.Translate(-w/2, -h/2)
+
+	tile := DrawTile()
+
+	for _, t := range data.Animals[12].Pattern {
+		DrawTileWithQR(dc, tile, int(t.DeltaQ), int(t.DeltaR))
+		switch t.Poi {
+		case types.POI_POIWater:
+			DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorBlue], 0), int(t.DeltaQ), int(t.DeltaR))
+		case types.POI_POIField:
+			DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorYellow], 0), int(t.DeltaQ), int(t.DeltaR))
+		case types.POI_POIBuilding:
+			for layer := range t.Height {
+				DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorRed], int(layer)), int(t.DeltaQ), int(t.DeltaR))
+			}
+		case types.POI_POIMountain:
+			for layer := range t.Height {
+				DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorGray], int(layer)), int(t.DeltaQ), int(t.DeltaR))
+			}
+		case types.POI_POITree:
+			for layer := range t.Height {
+				if layer != t.Height-1 {
+					DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorBrown], int(layer)), int(t.DeltaQ), int(t.DeltaR))
+				} else {
+					DrawTileWithQR(dc, DrawToken(colors[types.Color_ColorGreen], int(layer)), int(t.DeltaQ), int(t.DeltaR))
+				}
+			}
+		}
+	}
+
+	dc.SavePNG("../../output/card.png")
 }
 
 func PrintCard(card *types.Card) string {
--- go.mod
+++ go.mod
@@ -4,4 +4,9 @@ go 1.23.2
 
 require google.golang.org/protobuf v1.36.6
 
-require gopkg.in/yaml.v3 v3.0.1 // indirect
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.27.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
